e066dc15eb81782cc544e10aa8bcea9ca8caf16b
samuelmalik <samuelmalik9@gmail.com>
2022-09-27 19:38:34 +0200

    added function returning longest substring

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int j = 0;
+            int i = 0;
+            string longestSubstring = "";
+
+            for (i = 1; i <= str1.Length; i++)
+            {
+                for (j = 0; j <= str1.Length - i; j++)
+                {
+                    if (str2.Contains(str1.Substring(j, i)))
+                    {
+                        longestSubstring = str1.Substring(j, i);
+                    }
+                }
+            }
+            return longestSubstring;
         }
     }
 }
+                   
+                    
